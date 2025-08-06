96ad461c93fa01794e8566f9058205fba0ae2e97
dheeraj <dheerajkumar12757@gmail.com>
2025-08-07 00:08:07 +0530

    C20: Add Description Component in Product Page
    Add Description Component in porduct page also add the style sheet.

---
+++ frontend/src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import './DescriptionBox.css'
+
+const DescriptionBox = () => {
+  return (
+    <div className='descriptionbox'>
+        <div className="descriptionbox-navigator">
+            <div className="description-nav-box">Description</div>
+            <div className="description-nav-box fade">Reviews (122)</div>
+        </div>
+        <div className="descriptionbox-description">
+            <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, earum. Architecto, culpa! Perferendis nesciunt explicabo maiores cupiditate odio obcaecati exercitationem vero, ipsam fugit similique. Totam debitis repellat ea cumque? Iure.
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ullam nulla iste, nam assumenda repellat necessitatibus ratione fugiat facilis delectus cum ex maiores dolore voluptatibus sit adipisci, veritatis temporibus repellendus molestiae?
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi aperiam distinctio, ea voluptatibus blanditiis architecto deserunt aut voluptatum, sapiente beatae, rem possimus nulla dignissimos sed a odio aliquam non? Numquam.
+            </p>
+        </div>
+    </div>
+  )
+}
+
+export default DescriptionBox
