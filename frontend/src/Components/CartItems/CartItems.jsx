9986aa67d89465cbeff8195a26eadc6f12e15e7f
dheeraj <dheerajkumar12757@gmail.com>
2025-08-11 18:58:38 +0530

    C23: Add extra details in cart page
    update navbar
    update cart page

--- frontend/src/Components/CartItems/CartItems.jsx
+++ frontend/src/Components/CartItems/CartItems.jsx
@@ -4,7 +4,7 @@ import { ShopContext } from '../../Context/ShopContext'
 import remove_icon from '../Assets/cart_cross_icon.png'
 
 const CartItems = () => {
-    const {all_product, cartItems, removeFromCart} = useContext(ShopContext);
+    const {all_product, cartItems, removeFromCart, getTotalCartAmount} = useContext(ShopContext);
   return (
     <div className='cartitems'>
         <div className="cartitems-format-main">
@@ -32,6 +32,35 @@ const CartItems = () => {
             }
             return null;
         })}
+        <div className="cartitems-down">
+            <div className="cartitems-total">
+                <div>
+                    <h1>Cart Total</h1>
+                    <div className="cartitems-total-item">
+                        <p>SubTotal</p>
+                        <p>${getTotalCartAmount()}</p>
+                    </div>
+                    <hr />
+                    <div className="cartitems-total-item">
+                        <p>Shipping Fee</p>
+                        <p>Free</p>
+                    </div>
+                    <hr />
+                    <div className="cartitems-total-item">
+                        <h3>Total</h3>
+                        <h3>${getTotalCartAmount()}</h3>
+                    </div>
+                </div>
+                <button>PROCEED TO CHECKOUT</button>
+            </div>
+            <div className="cartitems-promocode">
+                <p>If you have a promo code, Enter it here</p>
+                <div className="cartitems-promobox">
+                    <input type="text" placeholder='promo code' />
+                    <button>Submit</button>
+                </div>
+            </div>
+        </div>
     </div>
   )
 }
