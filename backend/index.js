09eb12099e7fce4b97425db353acdcdc64b71b2f
dheeraj <dheerajkumar12757@gmail.com>
2025-08-14 13:51:04 +0530

    C32: Start express and add mogodb database

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,32 @@
 // index.js - entry file for backend
+
+const port = 4000;
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const multer = require("multer");
+const path = require("path");
+const cors = require("cors");
+
+app.use(express.json());
+app.use(cors());
+
+// Database Connection with MongoDB
+mongoose.connect("mongodb+srv://dheeraj321:patna757@cluster0.uvafpxf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+
+// API Creation
+
+app.get("/", (req, res)=> {
+    res.send("Express app is running");
+})
+
+
+app.listen(port, (error) => {
+    if(!error) {
+        console.log("Server Running on port " + port);
+    }
+    else {
+        console.log("Error : " + error);
+    }
+});
