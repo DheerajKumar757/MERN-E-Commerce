9b16ca2e3b68afce6e66149e264f80139916efd6
dheeraj <dheerajkumar12757@gmail.com>
2025-08-14 14:56:34 +0530

    C33: Add api to upload file to the server
    use multer, diskstorage to upload file

--- backend/index.js
+++ backend/index.js
@@ -21,6 +21,29 @@ app.get("/", (req, res)=> {
     res.send("Express app is running");
 })
 
+// Image Storage Engine
+
+const storage_ = multer.diskStorage({
+    destination: './upload/images',
+    filename:(req, file, cb) => {
+        return cb(null, `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`);
+    }
+})
+
+const upload = multer({storage:storage_});
+
+// Creating upload endpoint for images
+
+app.use('/images', express.static('upload/images'))
+
+app.post("/upload", upload.single('product'), (req, res)=>{
+    res.json({
+        success:1,
+        image_url:`http://localhost:${port}/images/${req.file.filename}`
+    })
+})
+
+
 
 app.listen(port, (error) => {
     if(!error) {
